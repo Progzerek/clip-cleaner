55081a5e517d1679d04b4793a2102d2b576dd1f7
Progzerek <nagybalint159@gmail.com>
2026-05-03 15:55:04 +0200

    refactored delete files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::io;
 use walkdir::WalkDir;
 use rayon::prelude::*;
+use std::time::Instant;
+
+#[derive(Debug)]
 
 struct Clip{
     name: String,
@@ -41,10 +44,30 @@ fn get_size(clips: &Vec<Clip>)-> f64{
     (total_size) as f64 / 1024.0 / 1024.0
 }
 
-fn delete_clips(clips: &mut Vec<Clip>, target: f64){
+fn calculate_deleted(clip_path: &str, target: f64)-> Vec<Clip>{
+    let mut to_delete_clips = read_clips(clip_path);
+    to_delete_clips.sort_by(|a, b| a.date_modified.cmp(&b.date_modified));
+    let mut current_size = get_size(&to_delete_clips);
+    to_delete_clips.retain(|clip| {
+        if current_size > target{
+            if clip.length < 120.0{false}
+            else {
+                current_size -= ((clip.size) as f64)/1024.0/1024.0;
+                true
+            }
+        }
+        else {
+            false
+        }
+    });
+    to_delete_clips
+
+}
+/*
+fn delete_clips(mut clips: Vec<Clip>, target: f64){
     let mut i = 0;
     while i < clips.len() {
-        let size = get_size(clips);
+        let size = get_size(&clips);
         if size > target {
             if clips[i].length < (120) as f64{
                 i += 1;
@@ -56,8 +79,19 @@ fn delete_clips(clips: &mut Vec<Clip>, target: f64){
         } else {
             break;
         }
+    }
 }
+*/
+fn delete_clips(clips: Vec<Clip>){
+    for file in clips{
+        match std::fs::remove_file(&file.path) {
+            Ok(_)=>println!("Deleted {}", &file.name),
+            Err(e)=>println!("Failed to delete {}:\n{}", &file.name, e)
+        }
+        
+    }
 }
+
 
 fn main() {
     println!("||Clip space manager||");
@@ -94,13 +128,24 @@ fn main() {
     let mut safety_check = String::new();
     io::stdin().read_line(&mut safety_check).expect("Failed to read line");
     if safety_check.trim() == "y"{
-        let mut clips = read_clips(&target_path);
-        clips.sort_by(|a, b| a.date_modified.cmp(&b.date_modified));
-        delete_clips(&mut clips, target_size_gb*1024.0);
-        let current_size = (get_size(&clips))/1024.0;
-        println!("\nRemaining space: {:.1}", current_size);
+        let start = Instant::now();
+        let to_delete = calculate_deleted(&target_path, target_size_gb*1024.0);
+        delete_clips(to_delete);
+
+        let current_size = (get_size(&read_clips(&target_path)))/1024.0;
+        println!("\nRemaining space: {:.1}GB", current_size);
+        let duration = start.elapsed();
+        println!("Time elapsed: {:?}", duration);
     }
     else {
         println!("Failed confirmaion");
     }
 }
+
+
+/*
+fn main() {
+    let deleted = calculate_deleted("D:/unknown", 2000.0);
+    delete_clips(deleted);
+}
+*/
